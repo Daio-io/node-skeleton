323c00e824670a327feafd8bbcb2ff16399e0ed1
David <dle.williams1@gmail.com>
2014-10-08 00:25:41 +0100

    Code tidy up and reorganisation of index.js. Added handlers folder to group routes logically

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var express = require('express');
-var http = require('http');
 var credentials = require('./lib/credentials.js');
 var app = express();
-var routes = require('./routes/routes.js'); // add app routes here
+require('./routes/routes.js')(app); // add app routes here
 
 var viewEngine = require('./lib/view_engine.js')(app); // set the view engine in here
 
@@ -12,48 +11,11 @@ app.set('port', process.env.PORT || 3000);
 var domain = require('./lib/domain.js')(app); // set up a domain for the app
 
 app.use(express.static(__dirname + '/public')); // serve this content to the client without special handling (static)
-var middleware = require('./lib/middleware.js')(app, express); // app middleware in here
+require('./lib/middleware.js')(app); // app middleware in here
 
-// creating a switch to add environment specifics such as different logging in this case
-switch (app.get('env')) {
-case 'development':
-    //compact colorful dev logging
-    app.use(require('morgan')('dev'));
-    break;
-case 'production':
-    //module express-logger supo=ports daily rotations
-    app.use(require('express-logger')({
-        path: __dirname + '/log/requests.log'
-    }));
-    break;
-}
+require('./lib/environment_context.js')(app);
 
-//** Routes **//
-app.get('/', routes.index);
-// redirect all others to the index
-app.get('*', routes.index);
-app.use(routes.error500);
-app.use(routes.error404);
+// ** UNCOMMENT BELOW ONCE YOU HAVE ADDED CONNECTION STRINGS FOR DATABASE **
+// var database = require('./lib/database_connection.js')(app, credentials);
 
-
-// var database = require('./lib/database_connection.js')(app, credentials); - uncomment this once you have added connection string
-
-function startServer() {
-    http.createServer(app).listen(app.get('port'), function () {
-        console.log('Express started on localhost:' + app.get('port') +
-            'app running in ' + app.get('env') +
-            '; press ctrl + c to quit');
-
-    });
-}
-
-if (require.main === module) {
-    //application run directly then start the server
-    startServer();
-
-} else {
-    // if the application is imported via require export fucntion 
-    // to create server add to exports
-    module.exports = startServer();
-
-}
+require('./lib/server.js')(app);
--- routes/routes.js
+++ routes/routes.js
@@ -1,22 +1,13 @@
-module.exports.index = function (req, res) {
+var main = require('./handlers/main.js');
 
-    res.render('home', {
-        welcomeMessage: 'Welcome!'
-    });
+module.exports = function (app) {
 
-};
-
-module.exports.error500 = function (err, req, res, next) {
-    // pass in err to log the error to the console
-    console.error(err.stack);
-    res.status(500);
-    res.render('500');
-
-};
-
-module.exports.error404 = function (req, res) {
-    // you have to explicitly set status to 404 otherwise express defaults to 200
-    res.status(404);
-    res.render('404');
+    //** Routes **//
+    app.get('/', main.index);
+    app.get('/page', main.anotherPage);
+    // redirect all others to the index
+    app.get('*', main.index);
+    app.use(main.error500);
+    app.use(main.error404);
 
 };
